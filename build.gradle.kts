ad96854b2928a8b95663a4ee86da8ecf2391c1c7
Whisperlyric <1814104304@qq.com>
2026-03-04 08:14:57 +0800

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,77 @@
+plugins {
+    id("fabric-loom") version "1.15-SNAPSHOT"
+    id("maven-publish")
+}
+
+val currentDate = java.time.LocalDate.now().format(java.time.format.DateTimeFormatter.ofPattern("yyMMdd"))
+version = project.property("mod_version") as String
+group = project.property("maven_group") as String
+
+base {
+    archivesName.set(project.property("archives_base_name") as String)
+}
+
+repositories {
+    maven {
+        url = uri("https://api.modrinth.com/maven")
+    }
+    maven {
+        url = uri("https://maven.terraformersmc.com/")
+    }
+}
+
+dependencies {
+    minecraft("com.mojang:minecraft:${project.property("minecraft_version")}")
+    mappings(loom.officialMojangMappings())
+    modImplementation("net.fabricmc:fabric-loader:${project.property("loader_version")}")
+    modImplementation(include("maven.modrinth:midnightlib:${project.property("midnightlib_version")}")!!)
+    modImplementation("net.fabricmc.fabric-api:fabric-api:${project.property("fabric_version")}")
+    modImplementation("com.terraformersmc:modmenu:${project.property("modmenu_version")}")
+}
+
+tasks.processResources {
+    inputs.property("version", project.version)
+    inputs.property("minecraft_version", project.property("minecraft_version"))
+    inputs.property("loader_version", project.property("loader_version"))
+    filteringCharset = "UTF-8"
+
+    filesMatching("fabric.mod.json") {
+        expand(
+            "version" to project.version,
+            "minecraft_version" to project.property("minecraft_version"),
+            "loader_version" to project.property("loader_version")
+        )
+    }
+}
+
+val targetJavaVersion = 21
+
+tasks.withType<JavaCompile>().configureEach {
+    options.encoding = "UTF-8"
+    if (targetJavaVersion >= 10 || JavaVersion.current().isJava10Compatible()) {
+        options.release.set(targetJavaVersion)
+    }
+}
+
+java {
+    val javaVersion = JavaVersion.toVersion(targetJavaVersion)
+    if (JavaVersion.current() < javaVersion) {
+        toolchain.languageVersion.set(JavaLanguageVersion.of(targetJavaVersion))
+    }
+    withSourcesJar()
+}
+
+tasks.jar {
+    from("LICENSE") {
+        rename { "${it}_${project.property("archives_base_name")}" }
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            artifactId = project.property("archives_base_name") as String
+            from(components["java"])
+        }
+    }
+}
