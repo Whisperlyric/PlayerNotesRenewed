27a49e292064be865d8aea374b1c9f1ee09630f5
Whisperlyric <1814104304@qq.com>
2026-03-17 01:38:19 +0800

    Comment out chat prefix and suffix configuration
    Commented out chat-related configuration entries for future implementation.

--- src/main/java/dev/wsplrc/playernotesrenewed/client/config/Config.java
+++ src/main/java/dev/wsplrc/playernotesrenewed/client/config/Config.java
@@ -13,6 +13,14 @@ public class Config extends MidnightConfig {
     @Entry(category = GENERAL) public static boolean changeTabOrderOwn = false;
     @Comment(category = GENERAL, centered = true) public static Comment nameTag;
     @Entry(category = GENERAL) public static boolean editNameTags = true;
+    
+    // TODO: 聊天栏显示前后缀功能待实现
+    // @Comment(category = GENERAL, centered = true) public static Comment chat;
+    // @Entry(category = GENERAL) public static boolean showPrefixInChat = true;
+    // @Entry(category = GENERAL) public static boolean showSuffixInChat = true;
+    // @Comment(category = GENERAL, centered = true) public static Comment style;
+    // @Entry(category = GENERAL) public static boolean styleAffectPlayerName = false;
+    
     @Comment(category = GENERAL, centered = true) public static Comment autoUpdate;
     @Entry(category = GENERAL, min = 1, max = 300) public static int autoUpdateInterval = 15;
 }
