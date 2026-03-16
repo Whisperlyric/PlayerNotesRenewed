ab874407ff34d1701e871eb4ae15c9b2a510c213
王碚 <1814104304@qq.com>
2026-03-17 04:14:40 +0800

    another try for chat

--- src/main/java/dev/wsplrc/playernotesrenewed/client/mixin/ChangeChat.java
+++ src/main/java/dev/wsplrc/playernotesrenewed/client/mixin/ChangeChat.java
@@ -3,9 +3,9 @@ package dev.wsplrc.playernotesrenewed.client.mixin;
 import dev.wsplrc.playernotesrenewed.client.config.Config;
 import dev.wsplrc.playernotesrenewed.client.utils.Utils;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.components.ChatComponent;
 import net.minecraft.client.multiplayer.PlayerInfo;
 import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.contents.TranslatableContents;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
@@ -13,11 +13,10 @@ import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-@Mixin(targets = "net.minecraft.client.gui.components.ChatComponent")
+@Mixin(value = ChatComponent.class, priority = 2000)
 public abstract class ChangeChat {
 
     private static final Pattern VANILLA_MESSAGE_FORMAT = Pattern.compile("^((-> )?\\[[^<]+] )?<([^>]*)>\\s.+$");
-    private static final Pattern PARSEABLE_MESSAGE_KEYS = Pattern.compile("chat.type.(text|team.(text|sent))");
 
     @ModifyVariable(
             method = "addMessage(Lnet/minecraft/network/chat/Component;Lnet/minecraft/network/chat/MessageSignature;Lnet/minecraft/client/GuiMessageTag;)V",
@@ -76,38 +75,9 @@ public abstract class ChangeChat {
         }
         
         String decoratedPlayerName = buildDecoratedPlayerName(playerName, prefixes, suffixes);
+        String newText = text.replace("<" + playerName + ">", "<" + decoratedPlayerName + ">");
         
-        if (message.getContents() instanceof TranslatableContents ttc && PARSEABLE_MESSAGE_KEYS.matcher(ttc.getKey()).matches()) {
-            return rebuildTranslatableMessage(ttc, decoratedPlayerName);
-        } else {
-            String newText = text.replace("<" + playerName + ">", "<" + decoratedPlayerName + ">");
-            return Component.literal(newText);
-        }
-    }
-    
-    private Component rebuildTranslatableMessage(TranslatableContents ttc, String decoratedPlayerName) {
-        boolean team = ttc.getKey().contains("team");
-        StringBuilder sb = new StringBuilder();
-        
-        if (team) {
-            if (ttc.getKey().endsWith("sent")) {
-                sb.append("-> ");
-            }
-            Object teamArg = ttc.getArgs()[0];
-            String teamName = (teamArg instanceof Component c) ? c.getString() : String.valueOf(teamArg);
-            sb.append(teamName).append(" ");
-        }
-        
-        sb.append("<").append(decoratedPlayerName).append("> ");
-        
-        int contentIndex = team ? 2 : 1;
-        if (ttc.getArgs().length > contentIndex) {
-            Object contentArg = ttc.getArgs()[contentIndex];
-            String content = (contentArg instanceof Component c) ? c.getString() : String.valueOf(contentArg);
-            sb.append(content);
-        }
-        
-        return Component.literal(sb.toString());
+        return Component.literal(newText);
     }
     
     private String buildDecoratedPlayerName(String playerName, String prefixes, String suffixes) {
